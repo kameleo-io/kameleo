02d2b4175e42b999cc2e90b06b90dafb28e832b2
Kameleo Team <62140734+kameleo-team@users.noreply.github.com>
2026-04-23 10:14:36 +0200

    ci: sync from masking-report @ 6905dd4 (#29)
    Co-authored-by: kameleo-team <kameleo-team@users.noreply.github.com>

--- masking-report/tests/terminate.kameleo.ts
+++ masking-report/tests/terminate.kameleo.ts
@@ -1,27 +1,9 @@
 import { KameleoLocalApiClient } from "@kameleo/local-api-client";
 import { test as setup } from "@playwright/test";
-import path from "path";
 
-import { KAMELEO_PORT, KAMELEO_VERSION } from "../config.ts";
-import { runCommand } from "../utils/common.ts";
+import { KAMELEO_PORT } from "../config.ts";
 
 setup("terminate Kameleo CLI", async () => {
     const kameleoClient = new KameleoLocalApiClient({ basePath: `http://localhost:${KAMELEO_PORT}` });
     await kameleoClient.general.terminateApplication();
-
-    if (process.platform == "linux") {
-        runCommand("docker", [
-            "run",
-            "--rm",
-            "--entrypoint",
-            "sh",
-            "-v",
-            `${path.resolve("dist/docker-data")}:/data`,
-            "--user",
-            "1001",
-            `kameleo/kameleo-app:${KAMELEO_VERSION}`,
-            "-c",
-            "'rm -rf /data/*'",
-        ]);
-    }
 });
