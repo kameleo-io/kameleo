fb28bc5608adbe183e276ca011c3d126ba45961a
kameleo-team <kameleo-team@users.noreply.github.com>
2026-06-26 11:46:19 +0000

    ci: sync from masking-report @ bba392d

--- masking-report/tests/sites/anti-bot/cloudflare-turnstile.spec.ts
+++ masking-report/tests/sites/anti-bot/cloudflare-turnstile.spec.ts
@@ -1,21 +1,7 @@
 import { expect, test } from "../../../utils/browserFixture.ts";
 
-test.describe(() => {
-    test.use({
-        browserSettings: {
-            additionalOptions: [{ key: "kameleo:forceOpenShadowRoot", value: true }],
-        },
-    });
-    test("Cloudflare Turnstile", async ({ page }) => {
-        await page.goto("https://2captcha.com/demo/cloudflare-turnstile");
-
-        const iframeLocator = page.frameLocator("#cf-turnstile iframe[src^='https://challenges.cloudflare.com']");
-        await iframeLocator.locator("text=Verify you are human").click();
-
-        await page.waitForTimeout(5_000); // for video presentation purposes
-
-        const successLocator = iframeLocator.getByText("Success!");
-        await successLocator.waitFor({ state: "attached" });
-        await expect(successLocator).toBeVisible();
-    });
+test("Cloudflare Challenge", async ({ page }) => {
+    await page.goto("https://2captcha.com/demo/cloudflare-turnstile-challenge");
+    await page.waitForTimeout(5_000); // wait for Cloudflare security verification
+    await expect(page).toHaveTitle("Cloudflare Challenge demo: Sample page with Cloudflare Challenge Turnstile");
 });
