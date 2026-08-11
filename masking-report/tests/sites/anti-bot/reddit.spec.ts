f666529c2e4c154e916c06d9729ea8d83e2899e0
kameleo-team <kameleo-team@users.noreply.github.com>
2026-08-11 06:05:06 +0000

    ci: sync from masking-report @ 56c3fd4

--- masking-report/tests/sites/anti-bot/reddit.spec.ts
+++ masking-report/tests/sites/anti-bot/reddit.spec.ts
@@ -1,7 +1,14 @@
 import { expect, test } from "../../../utils/browserFixture.ts";
 
-test("Reddit", async ({ page }) => {
-    await page.goto("https://www.reddit.com/r/popular/best/?geo_filter=us");
-    await page.waitForTimeout(5_000); // wait for the page to fully load
-    await expect(page).toHaveTitle("r/popular");
+test.describe(() => {
+    test.use({
+        browserSettings: async ({ browserProduct }, use) => {
+            await use(browserProduct === "firefox" ? undefined : { arguments: ["--disable-http2"] });
+        },
+    });
+    test("Reddit", async ({ page }) => {
+        await page.goto("https://www.reddit.com/r/popular/best/?geo_filter=us");
+        await page.waitForTimeout(5_000); // wait for the page to fully load
+        await expect(page).toHaveTitle("r/popular");
+    });
 });
